3eda745679154924b80e5a7964afc0059651afff
dexin <tanx3406@gmail.com>
2024-06-24 16:43:04 +0800

    header nav mobile

--- apple-replica/tailwind.config.js
+++ apple-replica/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightgray': '#f5f5f7',
+        'darkgray': '#1d1d1f',
+        'lightblue': '#0066cc',   
+
+       
+      },
+    },
+    fontFamily: {
+      sans: ['SF Pro Text', 'SF Pro Icons' , 'Helvetica Neue' , 'Helvetica,Arial' ,' sans-serif'],
+    },
+  }, 
+  colors: {
+    'darkgray': '#1d1d1f',   
   },
   plugins: [],
 }
